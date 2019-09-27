9a4cc1ea535192c90959eccc35af25cb2223f5e3
lintaowx <lintaox.wu@intel.com>
2019-09-27 21:59:39 +0800

    further complete

--- query.py
+++ query.py
@@ -4,8 +4,50 @@ from bs4 import BeautifulSoup
 from stations import stations
 import urllib3
 from collections import OrderedDict
-
+from openpyxl import Workbook
+import time
+import random
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+headers_firefox = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "Host": "kyfw.12306.cn",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0",
+    "Cookie": "JSESSIONID=C4502AA3D6A080A952510B96F4CF15CB; BIGipServerotn=1156579850.50210.0000; RAIL_EXPIRATION="
+              "1569796156959; RAIL_DEVICEID=nlIBK2z1jxKrLiRnOOa9nCkIAdqTldGiiaBLxqWQhP1qNscZdMowhjYKvyvyclrvFZkBtQV"
+              "zNi7P6Q34_W_25ilA7X6VQY5-AFIgAWcFsjuN4HImZfqYBbOkhrXDfeNlsg7gn9EQWv-i_I3ZFvAXJna0gtCnpx_I; "
+              "BIGipServerpool_passport=183304714.50215.0000; route=c5c62a339e7744272a54643b3be5bf64; "
+              "_jc_save_fromStation=%u5317%u4EAC%2CBJP; _jc_save_toStation=%u5E7F%u5DDE%2CGZQ; "
+              "_jc_save_fromDate=2019-09-26; _jc_save_toDate=2019-09-26; _jc_save_wfdc_flag=dc; "
+              "BIGipServerpassport=904397066.50215.0000"
+
+}
+
+headers_google = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Host": "kyfw.12306.cn",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
+    "Cookie": "JSESSIONID=2BE4CD1F9B04BFADD534E494AE14FA01; route=c5c62a339e7744272a54643b3be5bf64; _"
+              "jc_save_wfdc_flag=dc; RAIL_EXPIRATION=1569811344621; RAIL_DEVICEID=E9memzjv96LvvIx35PywH1XTeTcjd2r5m"
+              "Bx2GzPN833Ikco5hPPlS7N-nH3fUii7zRvMvOA_MIrAVZ7_cLo_slHqj9Crlh2aoUVS29798CSMFT5GUlgkM_n4ISARQqamK1s5a"
+              "jEC8NHpFF9K_HHYxRRtch53-FLh; _jc_save_fromStation=%u5317%u4EAC%2CBJP; BIGipServerpool_passport="
+              "183304714.50215.0000; BIGipServerotn=1473839370.50210.0000; _jc_save_toStation=%u6210%u90FD%2CCDW; "
+              "_jc_save_fromDate=2019-10-01; _jc_save_toDate=2019-09-27"
+
+}
 
 
 class QueryPassStation:
@@ -14,24 +56,6 @@ class QueryPassStation:
                'from_station_telecode={}&' \
                'to_station_telecode={}&' \
                'depart_date={}'
-    headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Cache-Control": "max-age=0",
-        "Connection": "keep-alive",
-        "Host": "kyfw.12306.cn",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0",
-        "Cookie": "JSESSIONID=C4502AA3D6A080A952510B96F4CF15CB; BIGipServerotn=1156579850.50210.0000; RAIL_EXPIRATION="
-                  "1569796156959; RAIL_DEVICEID=nlIBK2z1jxKrLiRnOOa9nCkIAdqTldGiiaBLxqWQhP1qNscZdMowhjYKvyvyclrvFZkBtQV"
-                  "zNi7P6Q34_W_25ilA7X6VQY5-AFIgAWcFsjuN4HImZfqYBbOkhrXDfeNlsg7gn9EQWv-i_I3ZFvAXJna0gtCnpx_I; "
-                  "BIGipServerpool_passport=183304714.50215.0000; route=c5c62a339e7744272a54643b3be5bf64; "
-                  "_jc_save_fromStation=%u5317%u4EAC%2CBJP; _jc_save_toStation=%u5E7F%u5DDE%2CGZQ; "
-                  "_jc_save_fromDate=2019-09-26; _jc_save_toDate=2019-09-26; _jc_save_wfdc_flag=dc; "
-                  "BIGipServerpassport=904397066.50215.0000"
-
-    }
 
     def __init__(self, train_no, from_station, to_station, date):
         self.train_no = train_no
@@ -44,13 +68,30 @@ class QueryPassStation:
         src_encode = stations[self.from_station]
         dst_encode = stations[self.to_station]
         url = self.root_url.format(self.train_no, src_encode, dst_encode, self.date)
-        response = requests.get(url, headers=self.headers, verify=False)
-        response.encoding = 'utf-8'
-        self.pass_train_infos = response.json()['data']['data']
+        response = request_url(url)
+        self.pass_train_infos = response['data']
         pass_station_names = []
         for station_info in self.pass_train_infos:
             pass_station_names.append(station_info['station_name'])
-        return pass_station_names
+        start_indx = pass_station_names.index(from_station)
+        end_indx = pass_station_names.index(to_station)
+        return pass_station_names[start_indx:end_indx + 1]
+
+
+def request_url(url, verify=False):
+    count = 1
+    while count:
+        try:
+            headers = random.sample([headers_firefox, headers_google], 1)
+            response = requests.get(url, headers=headers, verify=verify)
+            response.encoding = 'utf-8'
+            available_trains = response.json()['data']
+            count = 0
+        except Exception:
+            print("please wait for {} seconds, 12306 maybe happened a big bug.".format(2 * count))
+            time.sleep(2 * count)
+            count += 1
+    return available_trains
 
 
 class QueryTrain:
@@ -80,49 +121,62 @@ class QueryTrain:
     }
 
     def __init__(self):
-        self.train_infos = []
+        self.stations_re = dict(zip(stations.values(), stations.keys()))
 
-    def query(self, src, dst, date):
-        url = self.encode_url(src, dst, date)
-        response = requests.get(url, headers=self.headers, verify=False)
-        response.encoding = 'utf-8'
-        return self.analysis_html(response)
+    def query_train_information(self, from_station, to_station, date, train_name=None):
+        train_infos = []
+        url = self.encode_url(from_station, to_station, date)
+        response = request_url(url)
+        available_trains = response['result']
 
-    def analysis_html(self, response):
-        try:
-            available_trains = response.json()['data']['result']
-        except Exception:
-            print("please wait for moment, 12306 maybe happened a big bug.")
-            exit(0)
         available_trains = [i.split('|') for i in available_trains]
-        stations_re = dict(zip(stations.values(), stations.keys()))
         for raw_train in available_trains:
-            train_info = OrderedDict()
-            train_info['train_no'] = raw_train[2]
-            train_info['train_name'] = raw_train[3]
-            train_info['start_station'] = stations_re.get(raw_train[4])
-            train_info['end_station'] = stations_re.get(raw_train[5])
-            train_info['from_station'] = stations_re.get(raw_train[6])
-            train_info['to_station'] = stations_re.get(raw_train[7])
-            temp = raw_train[13]
-            train_info['start_time'] = temp[:4] + '-' + temp[4:6] + '-' + temp[6:]
-            train_info['departure_time'] = raw_train[8]
-            train_info['arrive_time'] = raw_train[9]
-            train_info['cost_time'] = raw_train[10]
+            if train_name is not None:
+                if train_name != raw_train[3]:
+                    continue
+            train_infos.append(self.train_information(raw_train, self.stations_re))
+        return train_infos
 
-            train_info['business_seat'] = raw_train[32]
-            train_info['first_seat'] = raw_train[31]
-            train_info['second_seat'] = raw_train[30]
-            train_info['advance_soft_sleep'] = raw_train[21]
-            train_info['soft_sleep'] = raw_train[23]
-            train_info['bullet_sleep'] = raw_train[33]
-            train_info['hard_sleep'] = raw_train[28]
-            train_info['soft_seat'] = raw_train[24]
-            train_info['hard_seat'] = raw_train[29]
-            train_info['no_seat'] = raw_train[26]
-            train_info['others'] = raw_train[22]
-            self.train_infos.append(train_info)
-            break
+    @staticmethod
+    def train_information(raw_train, stations_re):
+        train_info = OrderedDict()
+        train_info['train_no'] = raw_train[2]
+        train_info['train_name'] = raw_train[3]
+        train_info['start_station'] = stations_re.get(raw_train[4])
+        train_info['end_station'] = stations_re.get(raw_train[5])
+        train_info['from_station'] = stations_re.get(raw_train[6])
+        train_info['to_station'] = stations_re.get(raw_train[7])
+        temp = raw_train[13]
+        train_info['start_time'] = temp[:4] + '-' + temp[4:6] + '-' + temp[6:]
+        train_info['departure_time'] = raw_train[8]
+        train_info['arrive_time'] = raw_train[9]
+        train_info['cost_time'] = raw_train[10]
+
+        train_info['type_business_seat'] = raw_train[32]
+        train_info['type_first_seat'] = raw_train[31]
+        train_info['type_second_seat'] = raw_train[30]
+        train_info['type_advance_soft_sleep'] = raw_train[21]
+        train_info['type_soft_sleep'] = raw_train[23]
+        train_info['type_bullet_sleep'] = raw_train[33]
+        train_info['type_hard_sleep'] = raw_train[28]
+        train_info['type_soft_seat'] = raw_train[24]
+        train_info['type_hard_seat'] = raw_train[29]
+        train_info['type_no_seat'] = raw_train[26]
+        train_info['others'] = raw_train[22]
+        return train_info
+
+    def query_ticket_number(self, from_station, to_station, date, train_name=None):
+        train_info = self.query_train_information(from_station, to_station, date, train_name)[0]
+        ticket_info = OrderedDict()
+        for key in train_info.keys():
+            if 'type_' not in key:
+                continue
+            ticket_info[key] = train_info[key]
+            ""
+            if train_info[key].isdigit():
+                print("=== train_no: {}, from {} to {}, type: {}, num: {} ===".
+                      format(train_name, from_station, to_station, key, train_info[key]))
+        return ticket_info
 
     def encode_url(self, src, dst, date):
         src_encode = stations[src]
@@ -131,11 +185,13 @@ class QueryTrain:
         return url
 
 
-if __name__ == '__main__':
+def get_all_infos(from_station, to_station, date):
+    all_tickets_infos = []
+
     qt = QueryTrain()
-    date = '2019-10-01'
-    qt.query('北京西', '信阳', date)
-    for train_info in qt.train_infos:
+    all_trains_infos = qt.query_train_information(from_station, to_station, date)
+    for train_info in all_trains_infos:
+        ticket_info = {}
         train_name = train_info['train_name']
         start_time = train_info['start_time']
         from_station = train_info['from_station']
@@ -144,3 +200,20 @@ if __name__ == '__main__':
         qps = QueryPassStation(train_no, from_station, to_station, start_time)
         pass_stations = qps.pass_station()
         print("train_name: {}, pass station is {}".format(train_name, pass_stations))
+        # query ticket information
+        for pass_station in pass_stations[1:]:
+            ticket = qt.query_ticket_number(from_station, pass_station, date, train_name)
+            train_info[pass_station] = ticket
+        ticket_info['train_name'] = train_info['train_name']
+        ticket_info['departure_time'] = train_info['departure_time']
+        ticket_info['arrive_time'] = train_info['arrive_time']
+        ticket_info['cost_time'] = train_info['cost_time']
+        ticket_info['pass_station'] = pass_stations
+        all_tickets_infos.append(ticket_info)
+
+
+if __name__ == '__main__':
+    date = '2019-10-01'
+    from_station = '北京西'
+    to_station = '信阳'
+    get_all_infos(from_station, to_station, date)
